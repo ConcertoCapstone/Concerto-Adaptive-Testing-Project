bb1566512aa4a838c0f42b955e74435594a82d07
andrewcantrell <andyroocantrell@gmail.com>
2021-11-11 16:44:30 -0800

    ya boi has something here

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')   # Add a touch of color
+sg.theme('Purple')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [
+    [sg.Text(text = "The New Title of This Page",text_color=('Blue'))],
+    [sg.Text('Some text on Row 1')],
+    [sg.Text('Enter something on Row 2'), sg.InputText()],
+    [sg.Button('Ok'), sg.Button('Cancel')]
+]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
